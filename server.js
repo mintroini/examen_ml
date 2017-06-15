da7c7cf16ae26b7a371b65e1033f7d7e434ed25b
Martin Introini <mintroini@Martins-MacBook-Pro.local>
2017-06-14 23:41:57 -0300

    removed unnecesary on server

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const sqlite = require('sql.js');
 const request = require('request');
-
 const _ = require('underscore');
-
-const filebuffer = fs.readFileSync('db/usda-nnd.sqlite3');
-
-const db = new sqlite.Database(filebuffer);
 
 const app = express();
 
@@ -20,56 +13,6 @@ app.set('port', (process.env.PORT || 3001));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
-
-const COLUMNS = [
-    'carbohydrate_g',
-    'protein_g',
-    'fa_sat_g',
-    'fa_mono_g',
-    'fa_poly_g',
-    'kcal',
-    'description',
-];
-app.get('/api/food', (req, res) => {
-    const param = req.query.q;
-
-    if (!param) {
-        res.json({
-            error: 'Missing required parameter `q`',
-        });
-        return;
-    }
-
-    // WARNING: Not for production use! The following statement
-    // is not protected against SQL injections.
-    const r = db.exec(`
-    select ${COLUMNS.join(', ')} from entries
-    where description like '%${param}%'
-    limit 100
-  `);
-
-    if (r[0]) {
-        res.json(
-            r[0].values.map((entry) => {
-                const e = {};
-                COLUMNS.forEach((c, idx) => {
-                    // combine fat columns
-                    if (c.match(/^fa_/)) {
-                        e.fat_g = e.fat_g || 0.0;
-                        e.fat_g = (
-                            parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-                        ).toFixed(2);
-                    } else {
-                        e[c] = entry[idx];
-                    }
-                });
-                return e;
-            }),
-        );
-    } else {
-        res.json([]);
-    }
-});
 
 app.get('/api/items/:id', (req, res, next) => {
     console.log("Call for: items/:id");
